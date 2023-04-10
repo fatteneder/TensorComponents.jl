0275315b078db1b3300f0bb1c75e7e450138aa6e
Florian Atteneder <florian.atteneder@gmail.com>
2023-04-10 20:42:07 +0200

    reorder tests; add todos

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,53 +8,6 @@ const TC = TensorComponents
 const TO = TensorOperations
 
 
-@testset "@index tests" begin
-
-    ### valid use
-
-    @test (TC.components(quote @index a = 2 end); true)
-    @test (TC.components(quote @index a, b = 2 end); true)
-    @test (TC.components(quote @index a, b, i123 = 2 end); true)
-    @test (TC.components(quote @index asdf = 1:123 end); true)
-    @test (TC.components(quote @index wasd, xyz = 3:4 end); true)
-    # multiple index definitions
-    @test (TC.components(quote @index a = 2; @index b = 2 end); true)
-    @test (TC.components(quote @index a = 2; @index b = 1:3 end); true)
-    @test (TC.components(quote @index a, c = 2; @index b = 1:2 end); true)
-    @test (TC.components(quote @index a, c = 2; @index b = 3 end); true)
-    @test (TC.components(quote @index a, c = 3:8; @index b, d = 123 end); true)
-    @test (TC.components(quote @index a, c = 2; @index b, d = 7:11; @index e, f, g = 1:5 end); true)
-
-
-    ### invalid use
-
-    # no indices
-    @test_throws ErrorException TC.components(quote @index end)
-    # invalid index name
-    @test_throws ErrorException TC.components(quote @index 1 = 2 end)
-    @test_throws ErrorException TC.components(quote @index 1k = 2 end)
-    @test_throws ErrorException TC.components(quote @index a, 123abc = 2 end)
-    @test_throws ErrorException TC.components(quote @index 1:4 end)
-    # no tuple syntax
-    @test_throws ErrorException TC.components(quote @index a b c = 123 end)
-    # duplicated index names
-    @test_throws ErrorException TC.components(quote @index a, a = 123 end)
-    @test_throws ErrorException TC.components(quote @index a, b = 123; @index b = 1 end)
-    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 1:4 end)
-    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 123 end)
-    # invalid index range
-    @test_throws ErrorException TC.components(quote @index a = 0 end)
-    @test_throws ErrorException TC.components(quote @index a = -1 end)
-    @test_throws ErrorException TC.components(quote @index a, b, c = 12.5 end)
-    @test_throws ErrorException TC.components(quote @index a, d, e, f = -123 end)
-    @test_throws ErrorException TC.components(quote @index a = 1:-5 end)
-    @test_throws ErrorException TC.components(quote @index a = 0:4 end)
-    @test_throws ErrorException TC.components(quote @index a, b, c = -1:-12 end)
-    @test_throws ErrorException TC.components(quote @index a, d, e, f = -1:2 end)
-
-end
-
-
 @testset "@components equations test" begin
 
     ### valid use
@@ -85,6 +38,16 @@ end
         A[i,j] = a * B[i,j]
         C[i,j] = cos(α) * D[i,j] + sin(β) * E[i,j]
     end)); true)
+    # TODO Support scalar LHSs.
+    # @test (eval(@components begin
+    #     a = b + 1
+    # end); true)
+    # # Impose symmetries on tensors to reduce number of operations
+    # @test (eval(@components begin
+    #     @index i, j = 4
+    #     @symmetry A[i,j] = A[j,i]
+    #     A[i,j] = a[i] * a[j]
+    # end); true)
 
 
     ### invalid use
@@ -141,6 +104,53 @@ end
         @index i, j, k = 3
         A[i,j] = B[i,j] + C[k]
     end))
+
+end
+
+
+@testset "@index tests" begin
+
+    ### valid use
+
+    @test (TC.components(quote @index a = 2 end); true)
+    @test (TC.components(quote @index a, b = 2 end); true)
+    @test (TC.components(quote @index a, b, i123 = 2 end); true)
+    @test (TC.components(quote @index asdf = 1:123 end); true)
+    @test (TC.components(quote @index wasd, xyz = 3:4 end); true)
+    # multiple index definitions
+    @test (TC.components(quote @index a = 2; @index b = 2 end); true)
+    @test (TC.components(quote @index a = 2; @index b = 1:3 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b = 1:2 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b = 3 end); true)
+    @test (TC.components(quote @index a, c = 3:8; @index b, d = 123 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b, d = 7:11; @index e, f, g = 1:5 end); true)
+
+
+    ### invalid use
+
+    # no indices
+    @test_throws ErrorException TC.components(quote @index end)
+    # invalid index name
+    @test_throws ErrorException TC.components(quote @index 1 = 2 end)
+    @test_throws ErrorException TC.components(quote @index 1k = 2 end)
+    @test_throws ErrorException TC.components(quote @index a, 123abc = 2 end)
+    @test_throws ErrorException TC.components(quote @index 1:4 end)
+    # no tuple syntax
+    @test_throws ErrorException TC.components(quote @index a b c = 123 end)
+    # duplicated index names
+    @test_throws ErrorException TC.components(quote @index a, a = 123 end)
+    @test_throws ErrorException TC.components(quote @index a, b = 123; @index b = 1 end)
+    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 1:4 end)
+    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 123 end)
+    # invalid index range
+    @test_throws ErrorException TC.components(quote @index a = 0 end)
+    @test_throws ErrorException TC.components(quote @index a = -1 end)
+    @test_throws ErrorException TC.components(quote @index a, b, c = 12.5 end)
+    @test_throws ErrorException TC.components(quote @index a, d, e, f = -123 end)
+    @test_throws ErrorException TC.components(quote @index a = 1:-5 end)
+    @test_throws ErrorException TC.components(quote @index a = 0:4 end)
+    @test_throws ErrorException TC.components(quote @index a, b, c = -1:-12 end)
+    @test_throws ErrorException TC.components(quote @index a, d, e, f = -1:2 end)
 
 end
 
