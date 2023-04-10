4b3ae83c3967e751eab6dfb7cad436ef5f93d188
Florian Atteneder <florian.atteneder@gmail.com>
2023-04-10 20:39:04 +0200

    Revert "use syntax for tests that assembles more actual usage"
    This reverts commit c7602ffcb5f5188356e6314505ac552f2b309233.

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,137 +8,139 @@ const TC = TensorComponents
 const TO = TensorOperations
 
 
-@testset "@components equations test" begin
-
-    ### valid use
-
-    # one equation
-    @test (eval(@components begin
-        @index i, j = 4
-        A[i,j] = B[i,j]
-    end); true)
-    # multiple equations
-    @test (eval(@components begin
-        @index i, j, k = 1:3
-        A[i,j] = B[i,j,k] * C[k]
-        D[i]   = A[i,j] * C[j]
-        E[i]   = B[i,k,k]
-    end); true)
-    # multiple equations with mixed indices
-    @test (eval(@components begin
-        @index i, j, k = 1:3
-        @index a, b, c = 3:5
-        A[i,j] = B[i,j,a] * C[a]
-        D[i]   = A[i,b] * C[b]
-        E[i,a] = B[i,a,b] * C[b]
-    end); true)
-    # Shuffle in some scalar variables, also with functions
-    @test (eval(@components begin
-        @index i, j = 4
-        A[i,j] = a * B[i,j]
-        C[i,j] = cos(α) * D[i,j] + sin(β) * E[i,j]
-    end); true)
-
-
-    ### invalid use
-
-    # undefined indices
-    @test_throws ErrorException @eval @components begin
-        @index i, j = 4
-        A[a,b] = B[b,a]
-    end
-    @test_throws ErrorException @eval @components begin
-        @index i, j = 4
-        A[j,i] = B[a,j,i]
-    end
-    @test_throws ErrorException @eval @components begin
-        @index i, j = 4
-        A[a,i] = B[a,i]
-    end
-    # undefined indices with multiple equations
-    @test_throws ErrorException @eval @components begin
-        @index i, j, k = 4
-        A[i,j] = B[i,j] * C[k,k]
-        D[a]   = B[i,j]
-    end
-
-    # name clashes
-    # between index and tensor
-    @test_throws ErrorException @eval @components begin
-        @index A, I, J = 1:3
-        A[I,J] = B[I,J]
-    end
-    # between index and scalar variable
-    @test_throws ErrorException @eval @components begin
-        @index I, J = 1:3
-        A[I,J] = I * B[I,J]
-    end
-    # between tensor and scalar variable
-    @test_throws ErrorException @eval @components begin
-        @index I, J = 1:3
-        A[I,J] = A * B[I,J]
-    end
-
-
-    @test_throws TO.IndexError @eval @components begin
-        @index i, j, k = 4
-        A[i,j] = B[i,j,j]
-    end
-    @test_throws TO.IndexError @eval @components begin
-        @index i, j = 1:4
-        A[i,j] = B[i,j,j]
-    end
-    @test_throws TO.IndexError @eval @components begin
-        @index i, j, k = 3
-        A[i,j] = B[i,j] + C[k]
-    end
-
-end
-
-
 @testset "@index tests" begin
 
     ### valid use
 
-    @test (eval(@components begin @index a = 2 end); true)
-    @test (eval(@components begin @index a, b = 2 end); true)
-    @test (eval(@components begin @index a, b, i123 = 2 end); true)
-    @test (eval(@components begin @index asdf = 1:123 end); true)
-    @test (eval(@components begin @index wasd, xyz = 3:4 end); true)
+    @test (TC.components(quote @index a = 2 end); true)
+    @test (TC.components(quote @index a, b = 2 end); true)
+    @test (TC.components(quote @index a, b, i123 = 2 end); true)
+    @test (TC.components(quote @index asdf = 1:123 end); true)
+    @test (TC.components(quote @index wasd, xyz = 3:4 end); true)
     # multiple index definitions
-    @test (eval(@components begin @index a = 2; @index b = 2 end); true)
-    @test (eval(@components begin @index a = 2; @index b = 1:3 end); true)
-    @test (eval(@components begin @index a, c = 2; @index b = 1:2 end); true)
-    @test (eval(@components begin @index a, c = 2; @index b = 3 end); true)
-    @test (eval(@components begin @index a, c = 3:8; @index b, d = 123 end); true)
-    @test (eval(@components begin @index a, c = 2; @index b, d = 7:11; @index e, f, g = 1:5 end); true)
+    @test (TC.components(quote @index a = 2; @index b = 2 end); true)
+    @test (TC.components(quote @index a = 2; @index b = 1:3 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b = 1:2 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b = 3 end); true)
+    @test (TC.components(quote @index a, c = 3:8; @index b, d = 123 end); true)
+    @test (TC.components(quote @index a, c = 2; @index b, d = 7:11; @index e, f, g = 1:5 end); true)
 
 
     ### invalid use
 
     # no indices
-    @test_throws ErrorException @eval @components begin @index end
+    @test_throws ErrorException TC.components(quote @index end)
     # invalid index name
-    @test_throws ErrorException @eval @components begin @index 1 = 2 end
-    @test_throws ErrorException @eval @components begin @index 1k = 2 end
-    @test_throws ErrorException @eval @components begin @index a, 123abc = 2 end
-    @test_throws ErrorException @eval @components begin @index 1:4 end
+    @test_throws ErrorException TC.components(quote @index 1 = 2 end)
+    @test_throws ErrorException TC.components(quote @index 1k = 2 end)
+    @test_throws ErrorException TC.components(quote @index a, 123abc = 2 end)
+    @test_throws ErrorException TC.components(quote @index 1:4 end)
     # no tuple syntax
-    @test_throws ErrorException @eval @components begin @index a b c = 123 end
+    @test_throws ErrorException TC.components(quote @index a b c = 123 end)
     # duplicated index names
-    @test_throws ErrorException @eval @components begin @index a, a = 123 end
-    @test_throws ErrorException @eval @components begin @index a, b = 123; @index b = 1 end
-    @test_throws ErrorException @eval @components begin @index b = 123; @index b = 1:4 end
-    @test_throws ErrorException @eval @components begin @index b = 123; @index b = 123 end
+    @test_throws ErrorException TC.components(quote @index a, a = 123 end)
+    @test_throws ErrorException TC.components(quote @index a, b = 123; @index b = 1 end)
+    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 1:4 end)
+    @test_throws ErrorException TC.components(quote @index b = 123; @index b = 123 end)
     # invalid index range
-    @test_throws ErrorException @eval @components begin @index a = 0 end
-    @test_throws ErrorException @eval @components begin @index a = -1 end
-    @test_throws ErrorException @eval @components begin @index a, b, c = 12.5 end
-    @test_throws ErrorException @eval @components begin @index a, d, e, f = -123 end
-    @test_throws ErrorException @eval @components begin @index a = 1:-5 end
-    @test_throws ErrorException @eval @components begin @index a = 0:4 end
-    @test_throws ErrorException @eval @components begin @index a, b, c = -1:-12 end
-    @test_throws ErrorException @eval @components begin @index a, d, e, f = -1:2 end
+    @test_throws ErrorException TC.components(quote @index a = 0 end)
+    @test_throws ErrorException TC.components(quote @index a = -1 end)
+    @test_throws ErrorException TC.components(quote @index a, b, c = 12.5 end)
+    @test_throws ErrorException TC.components(quote @index a, d, e, f = -123 end)
+    @test_throws ErrorException TC.components(quote @index a = 1:-5 end)
+    @test_throws ErrorException TC.components(quote @index a = 0:4 end)
+    @test_throws ErrorException TC.components(quote @index a, b, c = -1:-12 end)
+    @test_throws ErrorException TC.components(quote @index a, d, e, f = -1:2 end)
+
+end
+
+
+@testset "@components equations test" begin
+
+    ### valid use
+
+    # one equation
+    @test (eval(TC.components(quote
+        @index i, j = 4
+        A[i,j] = B[i,j]
+    end)); true)
+    # multiple equations
+    @test (eval(TC.components(quote
+        @index i, j, k = 1:3
+        A[i,j] = B[i,j,k] * C[k]
+        D[i]   = A[i,j] * C[j]
+        E[i]   = B[i,k,k]
+    end)); true)
+    # multiple equations with mixed indices
+    @test (eval(TC.components(quote
+        @index i, j, k = 1:3
+        @index a, b, c = 3:5
+        A[i,j] = B[i,j,a] * C[a]
+        D[i]   = A[i,b] * C[b]
+        E[i,a] = B[i,a,b] * C[b]
+    end)); true)
+    # Shuffle in some scalar variables, also with functions
+    @test (eval(TC.components(quote
+        @index i, j = 4
+        A[i,j] = a * B[i,j]
+        C[i,j] = cos(α) * D[i,j] + sin(β) * E[i,j]
+    end)); true)
+
+
+    ### invalid use
+
+    # undefined indices
+    @test_throws ErrorException TC.components(quote
+        @index i, j = 4
+        A[a,b] = B[b,a]
+    end)
+    @test_throws ErrorException TC.components(quote
+        @index i, j = 4
+        A[j,i] = B[a,j,i]
+    end)
+    @test_throws ErrorException TC.components(quote
+        @index i, j = 4
+        A[a,i] = B[a,i]
+    end)
+    # undefined indices with multiple equations
+    @test_throws ErrorException TC.components(quote
+        @index i, j, k = 4
+        A[i,j] = B[i,j] * C[k,k]
+        D[a]   = B[i,j]
+    end)
+
+    # name clashes
+    # between index and tensor
+    @test_throws ErrorException TC.components(quote
+        @index A, I, J = 1:3
+        A[I,J] = B[I,J]
+    end)
+    # between index and scalar variable
+    @test_throws ErrorException TC.components(quote
+        @index I, J = 1:3
+        A[I,J] = I * B[I,J]
+    end)
+    # between tensor and scalar variable
+    @test_throws ErrorException TC.components(quote
+        @index I, J = 1:3
+        A[I,J] = A * B[I,J]
+    end)
+
+
+    # invalid contractions (errors coming from TensorOperations.@tensor
+    # so we need to use @eval to force evaluation of @tensor)
+    @test_throws TO.IndexError eval(TC.components(quote
+        @index i, j, k = 4
+        A[i,j] = B[i,j,j]
+    end))
+    @test_throws TO.IndexError eval(TC.components(quote
+        @index i, j = 1:4
+        A[i,j] = B[i,j,j]
+    end))
+    @test_throws TO.IndexError eval(TC.components(quote
+        @index i, j, k = 3
+        A[i,j] = B[i,j] + C[k]
+    end))
 
 end
 
