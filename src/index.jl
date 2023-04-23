2334302f7f82536f54252f30d307850d6f8606bd
Florian Atteneder <florian.atteneder@gmail.com>
2023-04-23 18:52:36 +0200

    iterator protocol for Index

--- src/index.jl
+++ src/index.jl
@@ -12,3 +12,12 @@ Index(i::Int) = Index(1:i)
 Base.view(t::AbstractArray, idxs::Union{Index,Int}...) = view(t, (i isa Index ? i.rng : i for i in idxs)...)
 
 Base.length(i::Index) = length(i.rng)
+
+start(idx::Index) = idx.rng.start
+stop(idx::Index) = idx.rng.stop
+start(idx::Int) = idx
+stop(idx::Int) = idx
+
+
+Base.iterate(i::Index) = iterate(i.rng)
+Base.iterate(i::Index,state) = iterate(i.rng,state)
